7be1c4a276607f9c36bb6f5ff9966ebd3c16a777
Terrance <khumaloterrance@gmail.com>
2021-05-10 20:33:08 +0200

    add registerservice worker

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { searchRobots } from "./reducers";
 import App from './contaners/app';
 // import reportWebVitals from './reportWebVitals';
 import { createLogger } from 'redux-logger';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const logger = createLogger();
 const store = createStore(searchRobots, applyMiddleware(logger));
@@ -25,5 +25,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-registerServiceWorker();
+serviceWorkerRegistration.register();
 // reportWebVitals();
